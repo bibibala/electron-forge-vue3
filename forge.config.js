6f3e9765c4019815f52ac7159b8a9a211db094a5
ZSQ <1140918095@qq.com>
2024-10-04 16:29:24 +0800

    releasev0.0.1

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    tmpdir: 'D:/project/.electron-tmp'
   },
   rebuildConfig: {},
   makers: [
